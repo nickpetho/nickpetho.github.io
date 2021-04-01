f754d5e89f96d8ef678e7e8f5c39ffcb86ff3a5f
Nick Petho <nick.petho@gmail.com>
2021-04-01 18:09:51 -0400

    deploy

---
+++ precache-manifest.653798f18241566af92f028c963bcbb8.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a10e4b021540472657d451f8269cb809",
+    "url": "/index.html"
+  },
+  {
+    "revision": "700862bd1cad88b3db60",
+    "url": "/static/css/main.2218996b.chunk.css"
+  },
+  {
+    "revision": "df26997aad2ca476f1e9",
+    "url": "/static/js/2.2195cbf1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2195cbf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "700862bd1cad88b3db60",
+    "url": "/static/js/main.ac14e74f.chunk.js"
+  },
+  {
+    "revision": "e772e3ee0db6f9829b44",
+    "url": "/static/js/runtime-main.fdaf22cd.js"
+  },
+  {
+    "revision": "7d25fdd3c4daff36a62ae9eb18b721ce",
+    "url": "/static/media/A-Star Map.7d25fdd3.png"
+  },
+  {
+    "revision": "f275363bcc671e66fe0e36ec3fe5c8f9",
+    "url": "/static/media/A-Star Map.f275363b.png"
+  },
+  {
+    "revision": "6206da3eb42087c5946d5dfb7f08e265",
+    "url": "/static/media/AppleLogo.6206da3e.svg"
+  },
+  {
+    "revision": "71e42b5a8d6c0d6628ec8840cc4030e9",
+    "url": "/static/media/Bootstrap 4.71e42b5a.svg"
+  },
+  {
+    "revision": "c64a068ead3cf3210c7608cea6018583",
+    "url": "/static/media/C++_Logo.c64a068e.svg"
+  },
+  {
+    "revision": "d3ad67fa720fd7aee8c322caff911082",
+    "url": "/static/media/CSS_Logo.d3ad67fa.svg"
+  },
+  {
+    "revision": "65aa0b061fe9bf7f1f17b975e3958298",
+    "url": "/static/media/C_Logo.65aa0b06.svg"
+  },
+  {
+    "revision": "bdaccca278378c33d51a37613e6acd09",
+    "url": "/static/media/C_Sharp_Logo.bdaccca2.svg"
+  },
+  {
+    "revision": "530516030e26a09ac38473bd5d8eff0e",
+    "url": "/static/media/Express_Logo.53051603.svg"
+  },
+  {
+    "revision": "64d5e98f1a220cdcad2b1a70d52ab669",
+    "url": "/static/media/Git_Logo.64d5e98f.svg"
+  },
+  {
+    "revision": "24845c5ca600cfab149e84387d826465",
+    "url": "/static/media/HTML5_Logo.24845c5c.svg"
+  },
+  {
+    "revision": "e82418cf11135044698778ead2dda97e",
+    "url": "/static/media/HomeIcon.e82418cf.svg"
+  },
+  {
+    "revision": "87711407412494d529d45ee76167583c",
+    "url": "/static/media/Javascript_Logo.87711407.svg"
+  },
+  {
+    "revision": "0b47e9e4dd3edf5b67fa10457dfbd98d",
+    "url": "/static/media/Jira_Logo.0b47e9e4.svg"
+  },
+  {
+    "revision": "8197a2a1847789bf33ebf3f365accaa1",
+    "url": "/static/media/LinuxHome.8197a2a1.svg"
+  },
+  {
+    "revision": "e8f474110d481aaf74c5455307d7c6f1",
+    "url": "/static/media/LinuxQuestionMark.e8f47411.svg"
+  },
+  {
+    "revision": "dfe23f20247f117a444515315f49155f",
+    "url": "/static/media/Login Wallpaper.dfe23f20.jpg"
+  },
+  {
+    "revision": "81c091c22a8095a56cb2db1ff0ee6166",
+    "url": "/static/media/MacFolder.81c091c2.svg"
+  },
+  {
+    "revision": "2c7e8a51ec44e85a08d0be6b722ae3fb",
+    "url": "/static/media/MacMagnifyingGlass.2c7e8a51.png"
+  },
+  {
+    "revision": "908ac92e139245457d3d68bc8385d846",
+    "url": "/static/media/MacOS Catalina.908ac92e.jpg"
+  },
+  {
+    "revision": "c7ba93854a592dd87cc4adc1e41d5c0a",
+    "url": "/static/media/MazeGame.c7ba9385.png"
+  },
+  {
+    "revision": "ba55cb4a0ed90ca00c121ffa94b39886",
+    "url": "/static/media/MazeGame1.ba55cb4a.png"
+  },
+  {
+    "revision": "f550e18593bd72565aa2edc82fb85441",
+    "url": "/static/media/MazeGame2.f550e185.png"
+  },
+  {
+    "revision": "2aa7f7b569fd6e9176f49dd31eaa970e",
+    "url": "/static/media/MongoDb_Logo.2aa7f7b5.svg"
+  },
+  {
+    "revision": "094505a8715b7003ef71714f558211e7",
+    "url": "/static/media/NPCW.094505a8.svg"
+  },
+  {
+    "revision": "f434bfbbffc44588572e13e1e113391a",
+    "url": "/static/media/Node.js_Logo.f434bfbb.svg"
+  },
+  {
+    "revision": "59b54d9ad44ec05d137b8b722aebf57a",
+    "url": "/static/media/PowerIcon.59b54d9a.svg"
+  },
+  {
+    "revision": "bf8dd5e66e98310317d2ebe9a9c188f0",
+    "url": "/static/media/PySnake(1).bf8dd5e6.png"
+  },
+  {
+    "revision": "d30f63f86992ddc641c6f12a6cbb0df8",
+    "url": "/static/media/PySnake(2).d30f63f8.png"
+  },
+  {
+    "revision": "23b761fd0c5495ac17fc8fa8e514b420",
+    "url": "/static/media/PySnake(3).23b761fd.png"
+  },
+  {
+    "revision": "762e05f9dc1010bdaf579d21da5563aa",
+    "url": "/static/media/PySnake(4).762e05f9.png"
+  },
+  {
+    "revision": "a4d373c3204c00d501633198cce0ad3b",
+    "url": "/static/media/PySnake.a4d373c3.png"
+  },
+  {
+    "revision": "51c2eab22c8994e70e7633979982b451",
+    "url": "/static/media/Python_Logo.51c2eab2.svg"
+  },
+  {
+    "revision": "4235a1de2b3c605abc89048fcb74ca81",
+    "url": "/static/media/RESTful_Logo.4235a1de.svg"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/static/media/React_Logo.9a28da9f.svg"
+  },
+  {
+    "revision": "cd6165ec2c8336ef71b6c7f539faef2e",
+    "url": "/static/media/SQL_Logo.cd6165ec.svg"
+  },
+  {
+    "revision": "470548b4a95cae966d0750d6720e4082",
+    "url": "/static/media/Sass_Logo.470548b4.svg"
+  },
+  {
+    "revision": "29f6cc9c01b48c3b4ce9175b3783314f",
+    "url": "/static/media/Segoe UI.29f6cc9c.ttf"
+  },
+  {
+    "revision": "fee4a97c88d736d38c6588e206583608",
+    "url": "/static/media/TAC_Employee.fee4a97c.jpg"
+  },
+  {
+    "revision": "6cdc1c67757fd70e7983b789885bda1b",
+    "url": "/static/media/TAC_Home.6cdc1c67.jpg"
+  },
+  {
+    "revision": "aecd8e8813921d7a1d8b6c8c9fd9d1f3",
+    "url": "/static/media/TAC_Home.aecd8e88.jpg"
+  },
+  {
+    "revision": "3323dd0202edcaf8d4392e994668959c",
+    "url": "/static/media/Tailwind_Logo.3323dd02.svg"
+  },
+  {
+    "revision": "cc54965ec257bd155246628c4c081889",
+    "url": "/static/media/Ubuntu.cc54965e.jpg"
+  },
+  {
+    "revision": "23d1fdd50793a0f55a7c86018a5c2362",
+    "url": "/static/media/Unity_Logo.23d1fdd5.svg"
+  },
+  {
+    "revision": "963fa81c6b7016042e510bb05f32d096",
+    "url": "/static/media/Waru.963fa81c.png"
+  },
+  {
+    "revision": "cf9db825657426b2be21145653a105aa",
+    "url": "/static/media/Waru.cf9db825.png"
+  },
+  {
+    "revision": "9c89c22676e16d03209e73b5345e1402",
+    "url": "/static/media/Win10UserIcon.9c89c226.svg"
+  },
+  {
+    "revision": "204f859a0738f14af409487ba5b777da",
+    "url": "/static/media/WindowXP.204f859a.jpg"
+  },
+  {
+    "revision": "9d40b75e453033c39529b5dc39d7a857",
+    "url": "/static/media/Windows10.9d40b75e.jpg"
+  },
+  {
+    "revision": "da288dceaafd7c97f1b09c594eac7868",
+    "url": "/static/media/Windows7.da288dce.jpg"
+  },
+  {
+    "revision": "ad76019da8a348a0f382488163cb1756",
+    "url": "/static/media/Windows7Logo.ad76019d.png"
+  },
+  {
+    "revision": "590ec4df884537580132a99bda2756e4",
+    "url": "/static/media/Windows95Logo.590ec4df.svg"
+  },
+  {
+    "revision": "ac460a41b7e487525e74c0eb958b76ee",
+    "url": "/static/media/WindowsXpLogo.ac460a41.png"
+  },
+  {
+    "revision": "221f5e17a93d44af75d825dbc113591c",
+    "url": "/static/media/XPLogOff.221f5e17.svg"
+  },
+  {
+    "revision": "38938581e4923621c2d0cc54e34c0ce5",
+    "url": "/static/media/XPMagnifyingGlass.38938581.png"
+  },
+  {
+    "revision": "df82ecc21e3f57c334fe9781bd86bc07",
+    "url": "/static/media/document-icon.df82ecc2.svg"
+  },
+  {
+    "revision": "b77d3a2c9279a565de7458efacb6f4ef",
+    "url": "/static/media/github-logo.b77d3a2c.svg"
+  },
+  {
+    "revision": "0061313271b96bb1d90f6c4d2c7cb786",
+    "url": "/static/media/jQuery_Logo.00613132.svg"
+  },
+  {
+    "revision": "6caabfeda9203134d4d3e3d889954ba5",
+    "url": "/static/media/linkedin-logo.6caabfed.svg"
+  },
+  {
+    "revision": "4709ed4a1e5131f10dbd1b2600ccae57",
+    "url": "/static/media/windows10Folder.4709ed4a.svg"
+  },
+  {
+    "revision": "62f68f288893b7f58844e03b61f045cf",
+    "url": "/static/media/windows10logo.62f68f28.svg"
+  }
+]);
